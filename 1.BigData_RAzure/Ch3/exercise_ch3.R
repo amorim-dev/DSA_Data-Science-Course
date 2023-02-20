d77e9a1fa1891d5951595c577000b81a61704cd4
Bianca Amorim <amorim.bia.dev@gmail.com>
2023-02-20 10:32:19 +0200

    exercise ch3 corrected

--- 1.BigData_RAzure/Ch3/exercise_ch3.R
+++ 1.BigData_RAzure/Ch3/exercise_ch3.R
@@ -9,6 +9,8 @@ getwd()
 
 # Exercise 1 - Search for the function that lets you list all the files in the working directory
 dir()
+list.files()
+
 
 # Exercise 2 - Create a dataframe from 3 vectors: one of characters, one logical, and one of numbers
 vec1 = c('char1', 'char2', 'char3')
@@ -18,6 +20,7 @@ vec3 = c(1, 2, 3)
 df = data.frame(vec1, vec2, vec3)
 df
 
+
 # Exercise 3 - Consider the array below. 
 # Create a loop that checks for numbers greater than 10 and prints the number and a message to the console.
 vec1 <- c(12, 3, 4, 19, 34)
@@ -26,8 +29,10 @@ vec1
 for(num in vec1){
   if(num > 10){
     print(num)
+    print("This element of the vector is greater then 10.")
   }
 }
+
 
 # Exercise 4 - Consider the list below. Create a loop that prints to the console each element of the list
 lst2 <- list(2, 3, 5, 7, 11, 13)
@@ -43,13 +48,14 @@ mat1
 mat2 <- t(mat1)
 mat2
 
-# Element-wise multiplication
+# Element-wise multiplication (Some algorithms works with this type of matrix multiplication)
 mat3 <- mat1 * mat2 
 mat3
 
 # Matrix multiplication (%*% are used for matrix multiplication)
 mat4 <- mat1 %*% mat2
 mat4
+
 
 # Exercise 6 - Create a vector, matrix, list, and dataframe and name each of the objects
 vec1 <- c(1, 2, 3)
@@ -68,6 +74,8 @@ df <- data.frame(c(8:10))
 df
 names(df) <- c('numbers')
 df
+# I could use 'colnames' and 'rownames' too. Only 'names' is to rename columns
+
 
 # Exercise 7 - Consider the matrix below. Assign NA values randomly to 50 elements of the array
 # Hint: use the sample() function
@@ -83,22 +91,37 @@ for (i in mat2){
 }
 mat2
 
+# A better way would be:
+mat3 <- matrix(1:90, 10)
+mat3
+mat3[sample(1:50, 10)] = NA
+mat3
+
+
 # Exercise 8 - For the matrix below, calculate the sum per row and per column
 mat1 <- matrix(c(1:50), nrow = 5, ncol = 5, byrow = T)
 mat1
 
 ?apply
 row_sum <- apply(mat1, 1, sum) # Sum by row
+row_sum
 column_sum <- apply(mat1, 2, sum) # Sum by column
+column_sum
 
-cbind(mat1, row_sum)
-rbind(mat1, column_sum)
+# Another way could be:
+rowSums(mat1)
+colSums(mat1)
 
 
 # Exercise 9 - For the vector below, sort the values in ascending order
 a <- c(100, 10, 10000, 1000)
 a
 sort(a)
+
+# I can use order if I need the indices
+order(a)
+a[order(a)]
+
 
 # Exercise 10 - Print to the console all elements of the matrix below that are greater than 15
 mat1 <- matrix(c(1:50), nrow = 5, ncol = 5, byrow = T)
